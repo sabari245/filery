aa719d42dfc96c5a6ec64356d19d390e01db931f
sabari245 <sabari2004@hotmail.com>
2023-05-05 19:17:39 +0530

    added landing page

---
+++ src/components/landingpage/index.tsx
@@ -0,0 +1,17 @@
+import Hero from "./hero";
+import Advantage from "./advantage";
+import Feature from "./feature";
+import Cta from "./cta";
+import Footer from "./footer";
+
+export default function LandingPage() {
+    return (
+        <>
+            <Hero />
+            <Feature />
+            <Advantage />
+            <Cta />
+            <Footer />
+        </>
+    )
+}
